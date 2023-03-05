94cc4c54360ebfa658435ae3add0c22d4c39a367
manishtmtmt <manisht.mt.mt@gmail.com>
2023-03-05 16:14:15 +0530

    added get req

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-import postRoutes from './routes/posts.js'
+import postRoutes from "./routes/posts.js";
 
 const PORT = process.env.PORT || 5001;
 
@@ -17,7 +17,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to Memories API.");
+});
+
+app.use("/posts", postRoutes);
 
 mongoose.set("strictQuery", true);
 
@@ -32,4 +36,3 @@ mongoose
     )
   )
   .catch((error) => console.log(`${error} did not connect`));
-
